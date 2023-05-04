1034df6215f2f402a13a4c83c383bc197ff27d83
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-05-04 01:41:13 +0100

    add const to widget

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:the_ceep_app/core/app_text.dart';
+import 'package:the_ceep_app/screens/home/widgets/home_list_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -38,7 +39,7 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
               ListView(
-                physics: NeverScrollableScrollPhysics(),
+                physics: const NeverScrollableScrollPhysics(),
                 shrinkWrap: true,
                 children: [
                   ...listCards
@@ -52,57 +53,3 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class ListCard extends StatelessWidget {
-  final Icon icon;
-  final AppText appText;
-
-  const ListCard({
-    Key? key, required this.icon, required this.appText
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
-      child: Card(
-        elevation: 5,
-        shadowColor: Colors.black45,
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Container(
-                      height: 25.h,
-                      width: 25.w,
-                      decoration:   BoxDecoration(
-                          color: Colors.purple[50],
-                          borderRadius: const BorderRadius.all(Radius.circular(5)),
-                      ),
-                      child: icon,
-                    ),
-                  ),
-                  appText
-                ],
-              ),
-              const Icon(Icons.arrow_back_ios_new_rounded, size: 18,)
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-List<ListCard> listCards = [
-  ListCard(icon: const Icon(Icons.person), appText: AppText(text: "Add Staff",size: 15,)),
-  ListCard(icon: const Icon(Icons.person_add_alt_rounded), appText: AppText(text: "Add Customers",size: 15,)),
-  ListCard(icon: const Icon(Icons.insert_chart), appText: AppText(text: "Create Invoice",size: 15,)),
-  ListCard(icon: const Icon(Icons.inventory_sharp), appText: AppText(text: "Inventory Management",size: 15,)),
-  ListCard(icon: const Icon(Icons.add_box), appText: AppText(text: "My Products",size: 15,)),
-];
